52409579b7979248c40efc149f295aa35f6a0396
TanakAiko <cherifmbaye02@gmail.com>
2023-10-13 10:25:35 +0000

    just for use

---
+++ open.sh
@@ -0,0 +1,13 @@
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 75
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.4
+firefox --new-window https://accounts.google.com/AccountChooser/signinchooser?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser&theme=glif
+sleep 2
+firefox https://discord.com/channels/@me
+sleep 2
+firefox https://chat.openai.com/
+sleep 2
+firefox --new-window https://learn.zone01dakar.sn/intra/dakar/profile
+sleep 2
+firefox https://learn.zone01dakar.sn/git/
+sleep 2
+firefox https://github.com/
